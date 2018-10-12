9e2530f7fbe98face5b908dd2b269da1d00d4ed0
Phillip Wilson <pswilso@us.ibm.com>
2018-10-12 11:28:32 -0500

    added calc and package.json files

---
+++ calc.js
@@ -0,0 +1,32 @@
+console.log("Unit Testing with NodeUnit Module");
+
+module.exports = {
+
+	add: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1 + val2;
+	},
+	subtract: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1 - val2;
+	},
+	multiply: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1 * val2;
+	},
+	divide: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1 / val2;
+	}
+};
+
+function checkArguments(val1, val2){
+
+	if(isNaN(val1) ){
+		throw new Error("Arg1 invalid");
+	}
+
+	if(isNaN(val2) ){
+		throw new Error("Arg2 invalid");
+	}
+};
